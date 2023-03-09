b5937248afff794d43e7950d5254d5909f198789
JoshKeoshkerian <JKeoshke3745@slps.org>
2023-03-09 09:39:59 -0600

    Add project files.

---
+++ Capstone Chapter 5 Project/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.rtbData = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblInvestAmt = New System.Windows.Forms.Label()
+        Me.lblPercent = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(902, 28)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'rtbData
+        '
+        Me.rtbData.Location = New System.Drawing.Point(30, 56)
+        Me.rtbData.Name = "rtbData"
+        Me.rtbData.Size = New System.Drawing.Size(266, 281)
+        Me.rtbData.TabIndex = 2
+        Me.rtbData.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(643, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(162, 29)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Investor Data"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(585, 98)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(157, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Investment Amount:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(585, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(181, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Percent on Investment:"
+        '
+        'lblInvestAmt
+        '
+        Me.lblInvestAmt.BackColor = System.Drawing.Color.White
+        Me.lblInvestAmt.Location = New System.Drawing.Point(791, 98)
+        Me.lblInvestAmt.Name = "lblInvestAmt"
+        Me.lblInvestAmt.Size = New System.Drawing.Size(100, 23)
+        Me.lblInvestAmt.TabIndex = 6
+        '
+        'lblPercent
+        '
+        Me.lblPercent.BackColor = System.Drawing.Color.White
+        Me.lblPercent.Location = New System.Drawing.Point(791, 132)
+        Me.lblPercent.Name = "lblPercent"
+        Me.lblPercent.Size = New System.Drawing.Size(100, 23)
+        Me.lblPercent.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(302, 52)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(271, 29)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Search Investor Name:"
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(320, 98)
+        Me.txtSearch.Multiline = True
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(237, 54)
+        Me.txtSearch.TabIndex = 9
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(902, 450)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblPercent)
+        Me.Controls.Add(Me.lblInvestAmt)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.rtbData)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents rtbData As RichTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblInvestAmt As Label
+    Friend WithEvents lblPercent As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtSearch As TextBox
+End Class
---
+++ Capstone Chapter 5 Project/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class frmMain
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        'declare open as new openFiledialog so that when the Open Submenu is click,
+        'the OpenfileDialog will appear.
+        Dim Open As New OpenFileDialog()
+        'it is declared as System input and output Streamreader
+        'it reads characters from a byte stream in a particular encoding
+        Dim myStreamReader As System.IO.StreamReader
+        'in an open dialog box, it will give an opening filter for the current filenames,
+        'or the save file types.   
+        Open.Filter = "Text [*.txt*]|*.txt|All Files [*.*]|*.*"
+        'it checks if the file exists or not
+        Open.CheckFileExists = True
+        'sets the openfile dialog name as "OpenFile"
+        Open.Title = "OpenFile"
+        Open.ShowDialog(Me)
+
+        Try
+            'it opens the selected file by the user
+            Open.OpenFile()
+            'opens the existing file
+            myStreamReader = System.IO.File.OpenText(Open.FileName)
+            'it reads the streams from current position to the end of position and display the result to RichTextBox as Text
+            rtbData.Text = myStreamReader.ReadToEnd()
+        Catch ex As Exception
+            'it will catch if any errors occurs
+            MsgBox(ex.Message, MsgBoxStyle.Information)
+        End Try
+    End Sub
+End Class
